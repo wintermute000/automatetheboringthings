3f4c0dc5f08b439b93fcf39d077000d7a053a548
Johann Lo <johann@o2networks.local>
2016-11-03 14:55:11 +1100

    first commit

--- listanswers.py
+++ listanswers.py
@@ -1,15 +1,39 @@
 import os
 import re
+import sys
 
-answers_regex = re.compile(r'.*answers.*')
+
+grep_txt = sys.argv[1]
+
+txt_regex = re.compile(r'.*.txt')
+grep_regex = re.compile(sys.argv[1])
 
 file_list = os.listdir('.')
-answers_list = []
 
+# Create list of all files in current directory that end in '.txt'
+# answers_list = []
 # for file in file_list:
 #     if answers_regex.search(file) is not None:
 #         answers_list.append(file)
 
-answers_list = [file for file in file_list if answers_regex.search(file) is not None]
+# Create list of all files in current directory that end in '.txt' - LIST COMPREHENSION VERSION
+txt_list = [file for file in file_list if txt_regex.search(file) is not None]
 
-print(answers_list)
+# Iterate over each file via readlines and track line numbers
+for txt in txt_list:
+    print ('---------')
+    parse_txt = open(txt)
+    print('Grepping file ' + txt)
+    linecount = 0
+    found = None
+    # Search for the expression and print if found
+    for line in parse_txt.readlines():
+        matching = grep_regex.findall(line)
+        linecount += 1
+        if len(matching) is not 0:
+            print('Line found containing "' + grep_txt + '" at line ' + str(linecount) + ': ' + matching[0])
+            found = True
+    if found == None:
+        print ('No Lines found containing "' + grep_txt)
+
+
